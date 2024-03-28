cb8a017ee8813a5f77ebe95d4101a273e046a49c
RENATO <RENATO@DESKTOP-RLBKK5M>
2024-03-28 11:47:06 -0300

    1

--- exer1/controller/threadscruzamento.java
+++ exer1/controller/threadscruzamento.java
@@ -1,9 +1,41 @@
 package controller;
 
-public class threadscruzamento {
+import java.util.concurrent.Semaphore;
 
-	public threadscruzamento() {
-		// TODO Auto-generated constructor stub
+public class threadscruzamento extends Thread{
+	private String sentido;
+	private Semaphore semaforo;
+	static int numero =0;
+	private int carro;
+	
+	public threadscruzamento( int carro, Semaphore semaforo) {
+		this.carro = carro;
+		this.semaforo = semaforo;
 	}
-
+	@Override
+	public void run() {
+		try {
+			semaforo.acquire();
+			travessia();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			semaforo.release();
+		}
+		super.run();
+	}
+	private void travessia() {
+		if(numero == 0) {
+			sentido = "direita";
+		} else if(numero == 1) {
+			sentido ="baixo";
+		} else if(numero == 2) {
+			sentido = "esquerda";
+		} else {
+			sentido = "cima";
+		}
+		System.err.println("O "+(carro+1) + " o. carro esta seguindo no sentido para " + sentido);
+		numero++;
+	}
+	
 }
--- exer1/view/cruzamento.java
+++ exer1/view/cruzamento.java
@@ -1,10 +1,19 @@
 package view;
 
+import java.util.concurrent.Semaphore;
+
+import controller.threadscruzamento;
+
 public class cruzamento {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Semaphore semaforo = new Semaphore(1);
+		
+		for(int i=0; i< 4; i++) {
+			Thread cruzamento = new threadscruzamento(i, semaforo);
+			cruzamento.start();
+		}
+		
 	}
 
 }
